e0062d1d1cbd3fef3289bb3f97c48c7a868d9dce
HY PARK <clay0517@naver.com>
2023-06-22 12:28:23 +0900

    이미지 주소 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import '../images/terry.png'
+
 function Main(props) {
   const {darkmode, img} = props;
   return (
     <main className={darkmode ? 'App darkmode' : 'App'}>
       <h1>Main</h1>
-      <img src={img ? 'src/images/terry.png' : 'src/images/Ter_20230616_154704346_01-removebg-preview.png'}/>
+      <img src={img ? 'terry.png' : 'Ter_20230616_154704346_01-removebg-preview.png'}/>
     </main>
   )
 }
