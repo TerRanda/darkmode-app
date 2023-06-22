a35297bd9fc60bae10c48b39a5ed762d8c8c4261
HY PARK <clay0517@naver.com>
2023-06-22 11:35:54 +0900

    img src

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ function Main(props) {
   return (
     <main className={darkmode ? 'App darkmode' : 'App'}>
       <h1>Main</h1>
-      <img src={img ? 'src/images/terry.png' : null}/>
+      <img src={img ? 'src/images/terry.png' : 'src/images/Ter_20230616_154704346_01-removebg-preview.png'}/>
     </main>
   )
 }
