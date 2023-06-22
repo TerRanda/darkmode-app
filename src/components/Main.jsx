a641a1aa3e7ccc9b946dda76d6df2538862e63e6
HY PARK <clay0517@naver.com>
2023-06-22 12:43:28 +0900

    img url 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ function Main(props) {
   return (
     <main className={darkmode ? 'App darkmode' : 'App'}>
       <h1>Main</h1>
-      <img src={img ? 'terry.png' : 'Ter_20230616_154704346_01-removebg-preview.png'}/>
+      <img src={img ? process.env.PUBLIC_URL + '/images/terry.png' : process.env.PUBLIC_URL + '/images/Ter_20230616_154704346_01-removebg-preview.png'}/>
     </main>
   )
 }
